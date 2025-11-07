0057c8cf2221479cf3621696402bddff949db3af
Max Nycolas <166265104+MaxNycolas@users.noreply.github.com>
2025-11-06 21:15:39 -0300

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,9 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  timezone: 'America/Sao_Paulo',
 });
+
 
 module.exports = db;
