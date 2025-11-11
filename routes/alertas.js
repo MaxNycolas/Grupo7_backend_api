67359cda844e49c66405b7023dcd3c829406cae4
Max Nycolas <166265104+MaxNycolas@users.noreply.github.com>
2025-11-10 21:35:42 -0300

    Update alertas.js

--- routes/alertas.js
+++ routes/alertas.js
@@ -4,7 +4,7 @@ const db = require('../db');
 
 // GET - listar alertas
 router.get('/', (req, res) => {
-  db.query('SELECT * FROM alertas ORDER BY data_hora DESC', (err, results) => {
+  db.query('SELECT * FROM alertas ORDER BY id DESC', (err, results) => {
     if (err) return res.status(500).json({ error: err });
     res.json(results);
   });
@@ -14,10 +14,14 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   const { device_id, mensagem_ia } = req.body;
 
-  db.query('INSERT INTO alertas (device_id, mensagem_ia) VALUES (?, ?)', [device_id, mensagem_ia], (err) => {
-    if (err) return res.status(500).json({ error: err });
-    res.status(201).json({ status: 'Alerta registrado com sucesso' });
-  });
+  db.query(
+    'INSERT INTO alertas (device_id, mensagem_ia) VALUES (?, ?)',
+    [device_id, mensagem_ia],
+    (err) => {
+      if (err) return res.status(500).json({ error: err });
+      res.status(201).json({ status: 'Alerta registrado com sucesso' });
+    }
+  );
 });
 
 module.exports = router;
